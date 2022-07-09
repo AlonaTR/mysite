cab80574f17879f660a6f2c9106690bd728f10a8
AlonaTR <alionatrubchaninova443@gmail.com>
2022-07-09 21:33:21 +0200

    css file"

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, converters
 from numpy import sign
 from .views import *
 
+
 urlpatterns = [
-    path('', index),
+    path('', index, name='horoscope_index'),
     path('<int:month>/<int:day>', month_day_horoscope),
     path('type/<str:type>/', type_of_horoscope, name='type_horoscope'),
     path('<int:sign>/', info_about_horoscope_number ),
